3a81f6997f92d55a3876c2d8c9197fcbb07bbb5b
thaddeusmbrown <thaddeusmbrown@gmail.com>
2022-01-03 12:17:39 -0600

    removed pry-byebug calls and linted with rubocop.  This replaced reduce with each_with_object, looked into differences in the methods to understand why this was done.

--- lib/substrings.rb
+++ lib/substrings.rb
@@ -1,19 +1,13 @@
 # frozen_string_literal: true
 
-require 'pry-byebug'
-
 def substrings(phrase, dictionary)
-  # split the incoming phrase to be searched through with dictionary
+  # split the incoming phrase to be searched through with dictionary, ignore uppercase
   phrase_array = phrase.downcase.split
 
-  #binding.pry
-
-  dictionary.reduce(Hash.new(0)) do |result, search_word|
+  # go through each word in the dictionary and see if it is a substring of each word in the target phrase
+  dictionary.each_with_object(Hash.new(0)) do |search_word, result|
     phrase_array.each do |word|
-      if word.include?(search_word)
-        result[search_word] += 1
-      end
+      result[search_word] += 1 if word.include?(search_word)
     end
-    result
   end
 end
